c417ac633cbe144c553135bb90e3b19d52ad63c1
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2025-03-05 18:30:52 +0300

    The Command example project has been completed.

---
+++ Command/Example_1/src/Lamba.java
@@ -0,0 +1,15 @@
+public class Lamba {
+    private int lamba;
+    public Lamba(){
+        lamba = 0;
+    }
+    public void lambaAc(){
+        System.out.println("Lamba açıldı.");
+    }
+    public void lambaKapa(){
+        System.out.println("Lamba kapandı.");
+    }
+    public int lambaDegeri(){
+        return lamba;
+    }
+}
---
+++ Command/Example_1/src/OncekiKanal.java
@@ -0,0 +1,16 @@
+public class OncekiKanal implements Command{
+    private Televizyon tv;
+    public OncekiKanal(Televizyon t){
+        tv = t;
+    }
+
+    @Override
+    public void Execute() {
+        tv.birOncekiKanal();
+        System.out.println("Geçerli Kanal: " + tv.gecerliKanal());
+    }
+    @Override
+    public String toString(){
+        return "Önceki Kanal";
+    }
+}
