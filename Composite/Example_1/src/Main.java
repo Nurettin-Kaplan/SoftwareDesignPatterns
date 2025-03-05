2d788601a73dd684f2966fa046b77118b769e0a6
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2025-03-05 17:54:08 +0300

    The Composite example project has been completed.

--- Composite/Example_1/src/Main.java
+++ Composite/Example_1/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bolum kitap = new Bolum("Programlamaya Giriş");
+        Bolum b1 = new Bolum("Giriş");
+        b1.Add(new Metin("hello, world"));
+        b1.Add(new Metin("diger programlar"));
+        Bolum b12 = new Bolum("Temel Programlama");
+        b12.Add(new Metin("değişkenler"));
+        b12.Add(new Metin("kontrol"));
+        b12.Add(new Metin("döngüler"));
+        b12.Add(new Metin("fonksiyonlar"));
+        b1.Add(b12);
+
+        Bolum b2 = new Bolum("Değişkenler");
+        b2.Add(new Resim("değişkenler.jpg"));
+        b2.Add(new Metin("int, doyble, float, boolean"));
+
+        kitap.Add(b1);
+        kitap.Add(b2);
+
+        yazdir(kitap);
+        System.out.println();
+        yazdir(b12);
+    }
+
+    public static void yazdir(Icerik icerik){
+        icerik.Operation();
     }
 }
